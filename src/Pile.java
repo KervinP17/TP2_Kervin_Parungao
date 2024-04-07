de432a94591a8faf35a90fdf24f2e0bb7006bd86
parungaokervin1@gmail.com <parungaokervin1@gmail.com>
2024-04-06 20:00:25 -0400

    6.3

--- src/Pile.java
+++ src/Pile.java
@@ -1,5 +1,9 @@
 public class Pile {
     private final int TAILLE_INITIALE = 4;
     private final int RATIO_AGRANDISSEMENT = 2;
+    private Gardien[] gardiens;
+    private int nbElements;
+    private int capacite;
 
+    // toSring
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -1,10 +1,21 @@
 public class Zoo {
     private String nom;
-
+    private Pile pileGardiens;
+    private File fileVisiteurs;
     private int nombreEnclos;
-
+    private Enclos[] lesEnclos;
     private int nombreTotalAnimaux;
 
+    public Zoo(String nom, Pile pileGardiens, File fileVisiteurs, int nombreEnclos, Enclos[] lesEnclos, int nombreTotalAnimaux) {
+        this.nom = nom;
+        this.pileGardiens = pileGardiens;
+        this.fileVisiteurs = fileVisiteurs;
+        this.nombreEnclos = nombreEnclos;
+        this.lesEnclos = lesEnclos;
+        this.nombreTotalAnimaux = nombreTotalAnimaux;
+    }
 
+    public boolean ajouterEnclos(Enclos[] lesEnclos) {
 
+    }
 }
