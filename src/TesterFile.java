50188743e958d279aeab3e053665d6c7cbeed816
parungaokervin1@gmail.com <parungaokervin1@gmail.com>
2024-04-08 21:07:01 -0400

    8.4

--- src/TesterFile.java
+++ src/TesterFile.java
@@ -7,9 +7,13 @@ class TesterFile {
     @Test
     void visiteurAge65() {
 
-        Zoo testZoo = new Zoo();
+        Zoo testZoo = new Zoo("testZoo");
 
-        Visiteur visiteur3 = new Visiteur("visiteur1", 65, new String[]{"thon", "perroquet"});
+        Visiteur visiteur0 = new Visiteur("visiteur0", 69, new String[]{"lion", "corneille"});
+        Visiteur visiteur1 = new Visiteur("visiteur1", 24, new String[]{"python", "zèbre"});
+        Visiteur visiteur2 = new Visiteur("visiteur2", 53, new String[]{"saumon", "marsouin"});
+        Visiteur visiteur3 = new Visiteur("visiteur3", 65, new String[]{"thon", "perroquet"});
+        Visiteur visiteur4 = new Visiteur("visiteur4", 19, new String[]{"alligator", "lion"});
 
         assertTrue();
     }
